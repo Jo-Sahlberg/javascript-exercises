d1758d893fca5d7fe538cf31e9728aedfeb2952b
Jo-Sahlberg <johanna.sahlb@gmail.com>
2024-07-06 11:35:12 +0200

    Complete reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedString = string.split("");
+    let reversedArray = reversedString.reverse();
+    let reversedArrayToString = reversedArray.join("");
 
+    return reversedArrayToString
 };
+
+console.log(reverseString("Hur mar du"));
 
 // Do not edit below this line
 module.exports = reverseString;
