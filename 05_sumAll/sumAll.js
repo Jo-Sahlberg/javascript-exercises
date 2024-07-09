97096e8eaaf719bd8cb1a811ca676c349455eca1
Jo-Sahlberg <johanna.sahlb@gmail.com>
2024-07-09 14:07:11 +0200

    Complete sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    let sum = 0;
+
+    if(b<a) {
+        let c = a;
+        a = b;
+        b = c;
+    } else if (b < 0 || a < 0 || typeof b !== "number" || typeof a !== "number") {
+        return 'ERROR';
+    }
+
+    for(let i=b; i >= a; i-- ) {
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
