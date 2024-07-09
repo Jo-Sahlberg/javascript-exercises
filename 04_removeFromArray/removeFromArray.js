03c14a713c4fed3c176ab8717c8134379ee9a2f9
Jo-Sahlberg <johanna.sahlb@gmail.com>
2024-07-09 13:47:58 +0200

    Complete removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, value, secondValue) {
+    const newArray = [];
+
+    for(const indexValue of array) {
+        if(indexValue !== value && indexValue != secondValue) {
+            newArray.push(indexValue);
+        }
+    };
+
+    return newArray;
 };
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
