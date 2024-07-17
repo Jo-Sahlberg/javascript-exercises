1298c2760ebfe409b150b930fa4502c0874a5dfd
Jo-Sahlberg <johanna.sahlb@gmail.com>
2024-07-17 16:17:40 +0200

    Complete tempConversion function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let convertedNum = (tempF - 32) * 5/9;
+  return convertedNum.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let convertedNum = tempC * (9/5) + 32;
+  return convertedNum.toFixed(1); 
 };
 
 // Do not edit below this line
