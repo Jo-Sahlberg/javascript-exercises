9aef3db192f2dfc057791932fe4390912f428072
Jo-Sahlberg <johanna.sahlb@gmail.com>
2024-08-08 14:16:55 +0200

    Complete calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for(let num of arr) {
+    sum += num;
+  }
+  return sum;	
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 1;
+  for (let num of arr) {
+    result *= num;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let result = 0;
+  for(let i = 0; i <= b; i++) {
+    result += a * a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let result = 1;
+  if(n === 0 || n === 1) {
+    return 1;
+  } else {
+    for (let i = n; i > 0; i--) {
+      result *= i;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
