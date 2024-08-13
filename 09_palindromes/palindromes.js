51abd8ed09dd746e96a146237ab49e4e054898a3
Jo-Sahlberg <johanna.sahlb@gmail.com>
2024-08-13 11:11:20 +0200

    Complete palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    let newString = []; 
+
+    for(char of str){
+        if(char === " " || char === "." || char === "," || char === "!"){
+        } else {
+            newString.push(char.toLowerCase());
+        }
+    } 
+    let revString = [...newString].reverse();
+
+    if(newString.join('') === revString.join('')) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
