75c1dfcf9f115079d884c98ee0d3d3c3acd71db4
Jo-Sahlberg <johanna.sahlb@gmail.com>
2024-08-18 17:21:05 +0200

    Complete findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+
+    const currentYear = new Date().getFullYear();
+
+    return arr.reduce((oldest, person) => {
+        if(!person.yearOfDeath) {
+            person.yearOfDeath = currentYear;
+        }
+
+        const age = person.yearOfDeath - person.yearOfBirth;
+
+        if(!oldest || age > oldest.yearOfDeath - oldest.yearOfBirth) {
+            return person;
+        }
+
+        return oldest;
+    }, null);
 
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
